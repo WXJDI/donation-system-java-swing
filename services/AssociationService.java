7ad2a7feb30b3fba2d60859f491f8858b9c1280a
AyKrimino <ayoub.krimi14@gmail.com>
2024-12-11 21:22:03 +0100

    feat(form-validations): created form validations for association form

--- services/AssociationService.java
+++ services/AssociationService.java
@@ -4,16 +4,24 @@ import dao.AssociationDAO;
 import models.Association;
 import models.User;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 public class AssociationService {
     private services.UserService userService = new services.UserService();
 
-    public Association registerAssociationUser(String username, String password, String email, String name, String location) {
-        User user = userService.registerUser(username, password, email);
-        if (user != null) {
-            Association association = new Association(0,name,location,user);
-            boolean associationCreated = AssociationDAO.addAssociation(association);
-            if (associationCreated)
-                return association;
+    public Association registerAssociationUser(String username, String password, String email, String name, String location) throws SQLIntegrityConstraintViolationException {
+        try {
+            User user = userService.registerUser(username, password, email);
+            if (user != null) {
+                Association association = new Association(0, name, location, user);
+                boolean associationCreated = AssociationDAO.addAssociation(association);
+                if (associationCreated)
+                    return association;
+            }
+        } catch (SQLIntegrityConstraintViolationException e) {
+            throw e;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return null;
     }
