c45ca54f05286c491a52cac55fc13a1cc6e18d41
AyKrimino <ayoub.krimi14@gmail.com>
2024-12-12 23:04:47 +0100

    feat(donation): created donation model

---
+++ models/Donation.java
@@ -0,0 +1,67 @@
+package models;
+
+public class Donation {
+    private int id;
+    private String type;
+    private String description;
+    private int quantity;
+    private boolean isAvailable;
+    private Donor donor;
+
+    public Donation(int id, String type, String description, int quantity, Donor donor) {
+        this.id = id;
+        this.type = type;
+        this.description = description;
+        this.quantity = quantity;
+        this.isAvailable = quantity > 0;
+        this.donor = donor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public Donor getDonor() {
+        return donor;
+    }
+
+    public void setDonor(Donor donor) {
+        this.donor = donor;
+    }
+}
